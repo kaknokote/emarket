289d16b243475a019320d119d0c48481fb31a9c2
Vladislav <kaknokote@gmail.com>
2025-07-30 15:27:03 +0300

    Доработать корзину

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useAuth } from '../../hooks';
 import { removeFromCart } from '../../store/slices/auth-slice';
+import { ProductCard } from '../products/product-card';
 
 export const Cart = () => {
 	const dispatch = useDispatch();
 	const { user, status, error } = useAuth();
 	const products = useSelector((state) => state.products.products);
+	let sum = 0;
 
 	if (status === 'loading') {
 		return (
@@ -42,41 +44,35 @@ export const Cart = () => {
 	return (
 		<div className="p-6">
 			<h2 className="text-4xl text-center font-semibold mb-6">Корзина</h2>
-			<div className="flex flex-col gap-4">
+			<div className="flex flex-wrap gap-4">
 				{user.cart.map((item) => {
 					const product = products.find(
 						(p) => p.id === item.productId,
 					);
+					sum += product.price;
 					return (
 						<div
 							key={item.productId}
-							className="flex justify-between items-center mb-4 border-b pb-4"
+							className="flex justify-between items-center mb-4 pb-4"
 						>
-							<div>
-								<p className="text-lg font-semibold">
-									{product?.name || 'Товар не найден'}
-								</p>
-								<p className="text-gray-600">
-									ID: {item.productId}
-								</p>
-								{product && (
-									<p className="text-emerald-600">
-										{product.price}₽
-									</p>
-								)}
+							<div className="flex flex-wrap w-[260px] justify-center">
+								<ProductCard product={product} />
+								<button
+									type="button"
+									onClick={() =>
+										dispatch(removeFromCart(item.productId))
+									}
+									className="p-2 mt-2 w-[250px] bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors duration-200"
+								>
+									Удалить
+								</button>
 							</div>
-							<button
-								type="button"
-								onClick={() =>
-									dispatch(removeFromCart(item.productId))
-								}
-								className="p-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors duration-200"
-							>
-								Удалить
-							</button>
 						</div>
 					);
 				})}
+				<div className="text-2xl text-center font-semibold mb-6">
+					Сумма корзины: {sum}₽
+				</div>
 			</div>
 		</div>
 	);
